5c1fdb273eb71f31c0e58d0df57e1944ff96d6b4
Collyz <m.mowla03151@gmail.com>
2022-04-04 11:31:48 -0400

    First commit and push

--- Code/Notepad.java
+++ Code/Notepad.java
@@ -3,40 +3,83 @@ package Code;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.IOException;
+import java.util.Locale;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.text.Document;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 //import javax.swing.filechooser.*;
 
-public class Notepad {
-    private final Color pastelBlue = new Color(209, 237, 255, 255);
-    private final Color pastelYellow = new Color(255, 233, 176, 255);
-    private final Color pastelGreen = new Color(185, 217, 176, 255);
-    private final Color pastelPink = new Color(255, 224, 249, 255);
-    private final Color pastelRed = new Color(255, 186, 186, 255);
-    private final Color offWhite = new Color(255, 250, 241,255);
+public class Notepad implements ActionListener, ComponentListener{
+    private JFrame frame;  //Container for everything
+    private JScrollPane scroll; //Wraps textPane text
+    private JMenuBar menuBar;
+    private JMenu fileMenu;
+    private JMenu editMenu;
+    private JMenu settingsMenu, changeColorMenu, changeCursor;
+    private JMenuItem saveFile, openFile, exit;  //File items
+    private JMenuItem cutItem, copyItem, pasteItem, selectAll, clear, undo, redo;  //Edit options
+    private JMenuItem blue, yellow, green, pink, red, white, black, gray;  //Settings Menu Colors
+    private JTextArea textArea;
+    //Undo Manager
+    private UndoManager manager = new UndoManager();
 
+    private final Color PASTEL_BLUE = new Color(209, 237, 255, 255);
+    private final Color PASTEL_YELLOW = new Color(255, 254, 176, 255);
+    private final Color PASTEL_GREEN = new Color(185, 217, 176, 255);
+    private final Color PASTEL_PURPLE = new Color(255, 224, 249, 255);
+    private final Color PASTEL_RED = new Color(255, 186, 186, 255);
+
+    /*
+       JFrame and its components declaration and initialization
+       The components include a JMenuBar along with its JMenus and JMenuItems
+       A JTextArea to allow for text editing
+        */
     public Notepad(){
-        /*
-        JFrame and its components declaration and initialization
-        The components include a JMenuBar along with its JMenus and JMenuItems
-        A JTextArea to allow for text editing
-         */
-        JFrame frame = new JFrame("Untitled - Note");
-        JScrollPane scroll = new JScrollPane();
-        JMenuBar menuBar = new JMenuBar();
-        JMenu fileMenu = new JMenu("File");
-        JMenu editMenu = new JMenu("Edit");
-        JMenu settingsMenu = new JMenu("Settings");
-        JMenu changeColor = new JMenu("Change Color");
-        JMenuItem saveFile = new JMenuItem("Save File");
-        JMenuItem openFile = new JMenuItem("Open File");
-        JMenuItem exit = new JMenuItem("Exit");
-        JMenuItem cutItem = new JMenuItem("Cut");
-        JMenuItem copyItem = new JMenuItem("Copy");
-        JMenuItem pasteItem = new JMenuItem("Paste Item");
-        JMenuItem selectAll = new JMenuItem("Select All");
-        JMenuItem clear = new JMenuItem("Clear");
+        //JFrame and its dimension
+        frame = new JFrame("Untitled - Note");
         Dimension initialSize = new Dimension(500, 400);
-        JTextArea textArea = new JTextArea();
+        frame.setSize(initialSize);
+        frame.setLayout(new BorderLayout());
+        //JMenu and its items
+        menuBar = new JMenuBar();
+        menuBar.setBounds(0, 0, 500, 20);
+        menuBar.setLocation(0, 0);
+        fileMenu = new JMenu("File");
+        editMenu = new JMenu("Edit");
+        settingsMenu = new JMenu("Settings");
+        changeColorMenu = new JMenu("Change Color");
+        changeCursor = new JMenu("Change Cursor");
+        saveFile = new JMenuItem("Save File");
+        openFile = new JMenuItem("Open File...");
+        exit = new JMenuItem("Exit");
+        cutItem = new JMenuItem("Cut");
+        copyItem = new JMenuItem("Copy");
+        pasteItem = new JMenuItem("Paste");
+        selectAll = new JMenuItem("Select All");
+        clear = new JMenuItem("Clear");
+        undo = new JMenuItem("Undo");
+        redo = new JMenuItem("Redo");
+        blue = new JMenuItem("Blue");
+        yellow = new JMenuItem("Yellow");
+        green = new JMenuItem("Green");
+        pink = new JMenuItem("Purple");
+        red = new JMenuItem("Red");
+        white = new JMenuItem("White");
+        black = new JMenuItem("Black");
+        gray = new JMenuItem("Gray");
+        //JTextArea and JScrollPane
+        textArea = new JTextArea();
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setSize(initialSize);
+        textArea.getDocument().addUndoableEditListener(manager);
+        scroll = new JScrollPane(textArea);
+        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 
         //JFrame monitor location of appearing
         GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
@@ -46,9 +89,200 @@ public class Notepad {
         Point spawnLocation = new Point(middle.x - (int)initialSize.getWidth() , middle.y - (int)initialSize.getHeight());
         frame.setLocation(spawnLocation);
 
-        //Menu bar drop-downs and contents
+    }
 
+    /*
+    Adds everything to the JFrame
+     */
+    public void run(){
+
+        //File menu items and action listeners 
+        fileMenu.add(saveFile);
+        fileMenu.add(openFile);
+        fileMenu.add(exit);
+        saveFile.addActionListener(this);
+        openFile.addActionListener(this);
+        exit.addActionListener(this);
+        //Edit menu items and action listeners
+        editMenu.add(cutItem);
+        editMenu.add(copyItem);
+        editMenu.add(pasteItem);
+        editMenu.add(selectAll);
+        editMenu.add(clear);
+        editMenu.add(undo);
+        editMenu.add(redo);
+        undo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, ActionEvent.CTRL_MASK)); //Shortcut for undo
+        redo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y, ActionEvent.CTRL_MASK));//Shortcut for redo
+        cutItem.addActionListener(this);
+        copyItem.addActionListener(this);
+        pasteItem.addActionListener(this);
+        selectAll.addActionListener(this);
+        clear.addActionListener(this);
+        undo.addActionListener(this);
+        redo.addActionListener(this);
+        //Settings menu items and action listeners
+        settingsMenu.add(changeColorMenu); //Adds submenu  - Change Color to settings menu
+        settingsMenu.add(changeCursor); //Adds submenu - Change Cursor to settings menu
+        changeColorMenu.add(blue);
+        changeColorMenu.add(yellow);
+        changeColorMenu.add(green);
+        changeColorMenu.add(pink);
+        changeColorMenu.add(red);
+        changeColorMenu.add(white);
+        changeColorMenu.add(black);
+        changeColorMenu.add(gray);
+        blue.addActionListener(this);
+        yellow.addActionListener(this);
+        green.addActionListener(this);
+        pink.addActionListener(this);
+        red.addActionListener(this);
+        white.addActionListener(this);
+        black.addActionListener(this);
+        gray.addActionListener(this);
+        //Adds all the menus to the menu bar
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(settingsMenu);
+
+        //Adding menu and JScrollPane to the frame
+        frame.add(menuBar, BorderLayout.NORTH);
+        frame.add(scroll, BorderLayout.CENTER);
+        //JFrame visibility and closing operation set
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 
     }
 
+    /**
+     * Invoked when an action occurs.
+     * @param e the event to be processed
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        //File menu options
+        if(e.getSource() == openFile){
+            try {
+                Runtime.getRuntime().exec("explorer.exe /select," + "Desktop");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        else if(e.getSource() == saveFile){
+            System.out.println(textArea.getText());
+        }
+        if(e.getSource() == exit){
+            manager.discardAllEdits();
+            frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+        }
+
+        //Edit menu options
+        if(e.getSource() == cutItem){
+            textArea.cut();
+        }
+        else if(e.getSource() == copyItem){
+            textArea.copy();
+        }
+        else if(e.getSource() == pasteItem){
+            textArea.paste();
+        }
+        else if(e.getSource() == selectAll){
+            textArea.selectAll();
+        }
+        else if(e.getSource() == clear){
+            textArea.setText("");
+        }
+        else if(e.getSource() == undo){
+            try {
+                if (manager.canUndo()) {
+                    manager.undo();
+                }
+            }catch(CannotUndoException exception){
+            }
+
+        }
+        else if(e.getSource() == redo){
+            try{
+                if(manager.canRedo()){
+                    manager.redo();
+                }
+            }catch(CannotRedoException exception){
+
+            }
+        }
+
+        //Settings menu options
+        if(e.getSource() == blue){
+            textArea.setBackground(PASTEL_BLUE);
+            textArea.setCaretColor(Color.BLACK);
+        }
+        else if(e.getSource() == yellow){
+            textArea.setBackground(PASTEL_YELLOW);
+            textArea.setCaretColor(Color.BLACK);
+        }
+        else if(e.getSource() == green){
+            textArea.setBackground(PASTEL_GREEN);
+            textArea.setCaretColor(Color.BLACK);
+        }
+        else if(e.getSource() == pink){
+            textArea.setBackground(PASTEL_PURPLE);
+            textArea.setCaretColor(Color.BLACK);
+        }
+        else if(e.getSource() == red){
+            textArea.setBackground(PASTEL_RED);
+            textArea.setCaretColor(Color.BLACK);
+        }
+        else if(e.getSource() == white){
+            textArea.setBackground(Color.WHITE);
+            textArea.setCaretColor(Color.BLACK);
+        }
+        else if(e.getSource() == black){
+            textArea.setBackground(Color.BLACK);
+            textArea.setCaretColor(Color.WHITE);
+        }
+        else if(e.getSource() == gray){
+            textArea.setBackground(Color.lightGray);
+            textArea.setCaretColor(Color.RED);
+        }
+    }
+
+    /**
+     * Invoked when the component's size changes.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void componentResized(ComponentEvent e) {
+
+    }
+
+    /**
+     * Invoked when the component's position changes.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void componentMoved(ComponentEvent e) {
+
+    }
+
+    /**
+     * Invoked when the component has been made visible.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void componentShown(ComponentEvent e) {
+
+    }
+
+    /**
+     * Invoked when the component has been made invisible.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void componentHidden(ComponentEvent e) {
+
+    }
 }
